67a0b9932f3f161add13254f0261933b34715420
Breana Dupree Jones <bdupreej@wellesley.edu>
2018-11-27 22:03:51 +0000

    Login working

--- app.py
+++ app.py
@@ -50,9 +50,74 @@ def register():
     return redirect(request.referrer)
     
     
-@app.route('/login/')
+@app.route('/login/', methods=['POST'])
 def login():
-    conn = getConn()
+    try:
+        username = request.form['login_username']
+        passwd = request.form['login_password']
+        # print(username)
+        conn = getConn()
+        curs = conn.cursor(MySQLdb.cursors.DictCursor)
+        curs.execute('SELECT hashed FROM userpass WHERE username = %s',
+                     [username])
+        row = curs.fetchone()
+        # print passwd
+        if row is None:
+            # Same response as wrong password, so no information about what went wrong
+            flash('login incorrect. Try again or join')
+            return redirect( url_for('home'))
+        hashed = row['hashed']
+        if hashed == passwd:
+            flash('successfully logged in as '+username)
+            session['username'] = username
+            session['logged_in'] = True
+            session['visits'] = 1
+            print(username)
+            return redirect( url_for('user', username=username) )
+        else:
+            flash('login incorrect. Try again or join')
+            return redirect( url_for('home'))
+    except Exception as err:
+        flash('form submission error '+str(err))
+        return redirect( url_for('home') )   
+@app.route('/user/<username>')
+def user(username):
+    try:
+        # don't trust the URL; it's only there for decoration
+        conn = getConn()
+        posts = sqlFunctions.getItemsAndUsers(conn)
+        if 'username' in session:
+            username = session['username']
+            session['visits'] = 1+int(session['visits'])
+            return render_template('main.html',posts=posts,
+                                   username=username,
+                                   visits=session['visits'])
+        else:
+            flash('you are not logged in. Please login or join')
+            return redirect( url_for('home') )
+    except Exception as err:
+        flash('some kind of error '+str(err))
+        return redirect( url_for('home') )
+
+@app.route('/logout/')
+def logout():
+    try:
+        if 'username' in session:
+            username = session['username']
+            session.pop('username')
+            session.pop('logged_in')
+            flash('You are logged out')
+            return redirect(url_for('home'))
+        else:
+            flash('you are not logged in. Please login or join')
+            return redirect( url_for('home') )
+    except Exception as err:
+        flash('some kind of error '+str(err))
+        return redirect( url_for('home') )
+
+    
+    
+    
 
 if __name__ == '__main__':
     app.debug = True
